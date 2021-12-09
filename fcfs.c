8ea9021a2c72583395d7ed4d8bbc42288c8078b5
Das-art <78835112+Das-art@users.noreply.github.com>
2021-12-09 09:57:53 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -19,3 +19,21 @@ initial=r[i];
 printf("totalhead%d",th);
 return 0;
 }
+
+/*....OUTPUT.....
+
+enter no:of sqncs
+7
+enter sqncs
+82
+170
+43
+140
+24
+16
+190
+enter head
+50
+totalhead 642
+
+*/
